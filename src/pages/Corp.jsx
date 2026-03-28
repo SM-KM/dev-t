052a2c9f867015876054c46e61ee61ca2d323e36
SMKM <svargas@desma.com.mx>
2026-03-27 19:52:55 -0600

    some shit

--- src/pages/Corp.jsx
+++ src/pages/Corp.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router";
 export default function Corp() {
+	const [form, setForm] = useState({
+		name: '',
+		email: '',
+		subject: '',
+		msg: '',
+	});
+
+	const handlech = (e) => {
+		const { name, value } = e.target;
+		setForm(prev => ({
+			...prev,
+			[name]: value
+		}))
+
+		console.log(form.name)
+	}
 	return (
 		<>
+			<div>
+				<div>
+					<input name="name" value={form.name} type="text" onChange={handlech} />
+					<input name="email" value={form.email} type="email" onChange={handlech} />
+				</div>
+			</div>
 			<h1>Corpo stuff, maybe just say that im cordial</h1>
 			<p><Link to="/">Bye bye</Link></p>
 		</>
