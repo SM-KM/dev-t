316e52b774437a3a7e93bd7ba8ee2c9e0411a368
SMKM <svargas@desma.com.mx>
2026-03-27 20:23:22 -0600

    done

--- src/pages/Corp.jsx
+++ src/pages/Corp.jsx
@@ -15,6 +15,18 @@ export default function Corp() {
 			[name]: value
 		}))
 	}
+
+	const handlemail = async (e) => {
+		e.preventDefault();
+		const data = await fetch('/api/server', {
+			method: "POST",
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ name: form.name, email: form.email, msg: form.msg })
+		})
+		const res = await data.json()
+		console.log(res)
+	}
+
 	return (
 		<>
 			<div>
@@ -22,9 +34,8 @@ export default function Corp() {
 					<input name="name" value={form.name} type="text" onChange={handlech} />
 					<input name="email" value={form.email} type="email" onChange={handlech} />
 				</div>
-
 				<textarea name="msg" value={form.msg} type="text" onChange={handlech} />
-				<button>Talk?</button>
+				<button onClick={handlemail}>Talk?</button>
 			</div>
 			<h1>Corpo stuff, maybe just say that im cordial</h1>
 			<p><Link to="/">Bye bye</Link></p>
